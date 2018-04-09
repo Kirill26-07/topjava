8dda2142a463f24689fb8f6b899ce88da7616288
dmitry <ovcharenkodmitry@gmail.com>
2018-04-10 01:03:50 +0300

    go to end task - DataJpaMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -3,5 +3,12 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    List<Meal> findByUserIdOrderByIdDesc(int userId);
+
+    Meal findByIdAndUserId(int id, int userId);
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -2,36 +2,58 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
     @Autowired
-    private CrudMealRepository crudRepository;
+    private CrudMealRepository repository;
+
+    @Autowired
+    private CrudUserRepository crudUserRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (meal.isNew()) {
+            meal.setUser(crudUserRepository.getOne(userId));
+            return repository.save(meal);
+        } else {
+            Meal m = repository.getOne(meal.getId());
+            if (m.getUser().getId() == userId) {
+                meal.setUser(crudUserRepository.getOne(userId));
+                return repository.save(meal);
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        if (Objects.nonNull(repository.findByIdAndUserId(id, userId))) {
+            repository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return repository.findByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return repository.findByUserIdOrderByIdDesc(userId);
     }
 
     @Override
