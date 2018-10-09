e5e89398ee41b31b262546742f8383025c3e48a3
Kirill <k.shiryaev@corp.rtln.ru>
2018-10-09 13:49:27 +0300

    Update Repository

--- src/main/java/ru/javawebinar/topjava/repository/InMamoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMamoryMealRepository.java
@@ -2,17 +2,34 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class InMamoryMealRepository {
 
-    private final Set<Meal> mealSet = new HashSet<>();
-    private final List<MealWithExceed> mealWithExceeds = new ArrayList<>();
+    private final List<Meal> meals;
 
+    public InMamoryMealRepository() {
+        meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+    }
 
+    public List<MealWithExceed> getMealWithExceeded() {
+         return MealsUtil.getFilteredWithExceededInOnePass(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+    }
 
 }
