159105722a34076407edff13edf1cb3157a44821
Kirill <k.shiryaev@corp.rtln.ru>
2018-10-26 20:18:07 +0300

    HW3 - Refactoring

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -2,11 +2,9 @@ DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START 100000;
-
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  id               SERIAL PRIMARY KEY,
   name             VARCHAR                 NOT NULL,
   email            VARCHAR                 NOT NULL,
   password         VARCHAR                 NOT NULL,
